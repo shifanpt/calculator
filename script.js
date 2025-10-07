bc2065ca07e24ad5ab93a9a4fdb7dda51abcacc8
shifanpt <shifanpt321@gmail.com>
2025-10-07 15:02:07 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-function clearScreen() {
-  document.getElementById("result").value = "";
+function appendNumber(number) {
+  document.getElementById('result').value += number;
+}
+
+function appendOperator(operator) {
+  document.getElementById('result').value += operator;
+}
+
+function calculateResult() {
+  let result = eval(document.getElementById('result').value);
+  document.getElementById('result').value = result;
+}
+
+function clearResult() {
+  document.getElementById('result').value = '';
 }
 
 function deleteLast() {
-  let result = document.getElementById("result").value;
-  document.getElementById("result").value = result.slice(0, -1);
-}
-
-function insert(char) {
-  document.getElementById("result").value += char;
-}
-
-function calculate() {
-  let result = document.getElementById("result").value;
-  try {
-    document.getElementById("result").value = eval(result);
-  } catch (e) {
-    document.getElementById("result").value = "Error";
-  }
+  let result = document.getElementById('result').value;
+  document.getElementById('result').value = result.slice(0, -1);
 }
